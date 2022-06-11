7b47c9ba555f6ecabc4630750fcd2e53681b69d5
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-06-11 16:40:07 +0900

    FIX: Todoheader test

--- cypress/e2e/Todo/TodoHeader.cy.ts
+++ cypress/e2e/Todo/TodoHeader.cy.ts
@@ -8,9 +8,12 @@ describe('todo header testing', () => {
   })
 
   it('Add new todo', () => {
-    cy.findByPlaceholderText('Enter new todo').should('be.visible')
-    cy.findByPlaceholderText('Enter new todo').clear().type('Study Cypress')
+    cy.findByPlaceholderText(/Enter new todo/i).should('be.visible')
+    cy.findByPlaceholderText(/Enter new todo/i)
+      .clear()
+      .type('Study Cypress')
     cy.findByRole('button', { name: /ADD/i, hidden: true }).click()
+    cy.findByPlaceholderText(/enter new todo/i).clear()
     cy.findByText(/Study cypress/i).should('be.visible')
   })
 })
