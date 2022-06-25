086d8f87e5d60b86587b1ec6b2c81e5cb9033ec9
joonyg-Park <joonyg@dwhale.kr>
2022-06-25 13:38:34 +0900

    FIX: Cypress eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,14 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'cypress'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'cypress/no-assigning-return-values': 'error',
+    'cypress/no-unnecessary-waiting': 'error',
+    'cypress/assertion-before-screenshot': 'warn',
+    'cypress/no-force': 'warn',
+    'cypress/no-async-tests': 'error',
+    'cypress/no-pause': 'error',
   },
 }
